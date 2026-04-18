4a6ec9e7bb4baa89e39cdca4bad9ece1b454911b
Sanket Pehere <105560181+sanketpehere@users.noreply.github.com>
2026-04-18 16:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// get current URL
+const currentURL = window.location.href;
+
+// calculate length
+const urlLength = currentURL.length;
+ 
+// show result
+alert(`The length of the URL is: ${urlLength}`);
